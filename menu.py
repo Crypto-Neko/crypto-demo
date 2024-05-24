3708f6735da74727c0b2f3835f16b1cdabe32c42
Math Akemi <objective.axioma@gmail.com>
2024-05-24 19:31:01 -0400

    Started working on the GUI menu to encrypt and decrypt messages.

---
+++ menu.py
@@ -0,0 +1,20 @@
+import tkinter
+import sys; sys.path.append("Ciphers")
+from shift_cipher import *
+from substitution_cipher import *
+from affine_cipher import *
+from vigenere_cipher import *
+
+#########################################################################################
+# Implements a GUI menu allowing the user to encrypt a message with a random or custom  #
+# key using the four crypto implementations in the Ciphers directory.                   #
+#                                                                                       #
+# 
+#
+#
+#
+#
+#
+#
+#########################################################################################
+class CryptoGUI
